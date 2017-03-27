6666f4214f86c18fe5b982bd383a8192d4b0e782
Guido Benedetti <Guido Benedetti>
2017-03-27 19:25:49 -0300

    Added auto

--- src/archivos/PersonaFromFile.java
+++ src/archivos/PersonaFromFile.java
@@ -1,5 +1,8 @@
 package archivos;
 
+import java.util.Scanner;
+
+import clases.Auto;
 import clases.Persona;
 
 public class PersonaFromFile {
@@ -7,6 +10,13 @@ public class PersonaFromFile {
 	public static void main(String[] args) {
 		Persona p = Persona.getPersonaFromString("Pepe,Palote,15");
 		System.out.println(p);
+		
+		Scanner sc = new Scanner(System.in); 
+		Persona p1 = Persona.getPersonaFromString(sc.next());
+		System.out.println(p1);
+		
+		Auto a = Auto.getAutoFromTextLine(sc.next());
+		System.out.println(a);
 	}
 
 }
---
+++ src/clases/Auto.java
@@ -0,0 +1,41 @@
+package clases;
+
+public class Auto {
+	private String nombre;
+	private Integer modelo;
+	private String marca;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getModelo() {
+		return modelo;
+	}
+	public void setModelo(Integer modelo) {
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public Auto(String nombre, Integer modelo, String marca) {
+		super();
+		this.nombre = nombre;
+		this.modelo = modelo;
+		this.marca = marca;
+	}
+	@Override
+	public String toString() {
+		return "Auto [nombre=" + nombre + ", modelo=" + modelo + ", marca=" + marca + "]";
+	}
+	
+	public static Auto getAutoFromTextLine(String line) {
+		String[] value = line.split(",");
+		Auto a = new Auto(value[0], Integer.parseInt(value[1]), value[2]);
+		return a;
+	}
+}
